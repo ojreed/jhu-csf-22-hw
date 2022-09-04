9331c56707ba3645b42d3fb56a011103af7df358
Owen <oreed2@jhu.edu>
2022-09-04 15:09:21 -0400

    debugging add

--- csf_assign01/fixedpoint.c
+++ csf_assign01/fixedpoint.c
@@ -175,9 +175,9 @@ Fixedpoint fixedpoint_add(Fixedpoint left, Fixedpoint right) {
     sum.whole = big.whole-little.whole;
     if (big.fractional < little.fractional) {//need carry in fractional side
       sum.whole -= 1;
-      sum.fractional = (uint64_t) ((1<<64)-1) - (little.fractional - big.fractional);//do I need the plus 1?
+      sum.fractional = ((uint64_t) (1<<64)) - (little.fractional - big.fractional);//do I need the plus 1?
     } else { //fractional component behaves as expected
-      sum.fractional = (uint64_t) big.fractional - little.fractional;
+      sum.fractional = big.fractional - little.fractional;
     }
     sum.flag = big.flag; //maintain sign of the larger magnitude component  
   }
