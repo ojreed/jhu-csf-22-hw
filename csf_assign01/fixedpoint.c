03a1eb87ef84b6ef7a98a4494ab56fc9dd763588
Owen <oreed2@jhu.edu>
2022-09-11 11:36:41 -0400

    debug make hex

--- csf_assign01/fixedpoint.c
+++ csf_assign01/fixedpoint.c
@@ -400,7 +400,7 @@ char *fixedpoint_format_as_hex(Fixedpoint val) {
     for(int i = 0; i < 16; i++) { //67
       uint64_t hex = 0;
       for(int j = 0; j < 4; j++){
-        printf("#%li BIT VAL: %li ",ptr, ((val.whole & ptr) >> back_shift));
+        printf("#%li BIT VAL: %li ",ptr, ((val.fractional & ptr) >> back_shift));
         hex += ((val.fractional & ptr) >> back_shift);
         ptr = (ptr >> 1);
       }
