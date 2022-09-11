983ffe3ae90ae1872bb6184a79455765de129f5a
Owen <oreed2@jhu.edu>
2022-09-10 20:04:40 -0400

    debug make hex

--- csf_assign01/fixedpoint.c
+++ csf_assign01/fixedpoint.c
@@ -355,7 +355,7 @@ char *fixedpoint_format_as_hex(Fixedpoint val) {
     printf("INSIDE NEG\n");
     s[string_ptr] = '-';
     string_ptr++;
-    printf("\n%s\n",*s);
+    printf("\n%s\n",s);
   }
   printf("NEG CHECKED\n");
   int ptr = (1<<63);
