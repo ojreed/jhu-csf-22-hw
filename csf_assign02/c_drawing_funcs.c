d49e397fccb719cac67aa996d8add6ddf192259f
Owen <oreed2@jhu.edu>
2022-09-14 19:50:36 -0400

    set pix

--- csf_assign02/c_drawing_funcs.c
+++ csf_assign02/c_drawing_funcs.c
@@ -33,6 +33,13 @@ uint32_t get_pix(struct Image *img, int32_t x, int32_t y) {
   return img->data[location]; 
 }
 
+void set_pix(struct Image *img, int32_t x, int32_t y, int32_t color) {
+  uint8_t rows = img->height;
+  uint8_t cols = img->width;
+  uint8_t location = y*cols + x;
+   img->data[location]; 
+}
+
 ////////////////////////////////////////////////////////////////////////
 // API functions
 ////////////////////////////////////////////////////////////////////////
