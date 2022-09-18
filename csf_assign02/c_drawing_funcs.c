c074bab026c1eba4a60b02f9bc293ac35dd431e7
Owen <oreed2@jhu.edu>
2022-09-18 16:28:40 -0400

    WIP bounds check

--- csf_assign02/c_drawing_funcs.c
+++ csf_assign02/c_drawing_funcs.c
@@ -28,9 +28,9 @@ uint32_t blur_colors(uint32_t foreground, uint32_t background) {
   final_color += a;
 
   for(int i = 1; i < 4; i++) {
-    f = (foreground | (255UL << 8*i)) >> 8*i;
-    b = (background | (255UL << 8*i)) >> 8*i;
-    final_color += (((a*f + (255 - a)*b)/255) << 8*i);
+    f = (foreground | (255UL << (8*i))) >> (8*i);
+    b = (background | (255UL << (8*i))) >> (8*i);
+    final_color += (((a*f + (255 - a)*b)/255) << (8*i));
   }
 
   return final_color;
