22c83fee40e273fed48ad32282c3bd8bcc085fc0
Owen <oreed2@jhu.edu>
2022-09-26 17:42:48 -0400

    testing

--- csf_assign02/test_drawing_funcs.c
+++ csf_assign02/test_drawing_funcs.c
@@ -77,7 +77,7 @@ void check_picture(struct Image *img, Picture *p) {
 }
 
 // prototypes of test functions
-////void test_draw_pixel(TestObjs *objs);
+void test_draw_pixel(TestObjs *objs);
 //void test_draw_rect(TestObjs *objs);
 //void test_draw_circle(TestObjs *objs);
 //void test_draw_circle_clip(TestObjs *objs);
@@ -99,7 +99,7 @@ int main(int argc, char **argv) {
   TEST_INIT();
 
   // TODO: add TEST() directives for your helper functions
-  TEST(test_draw_pixel);
+  ////TEST(test_draw_pixel);
   //TEST(test_draw_rect);
   //TEST(test_draw_circle);
   //TEST(test_draw_circle_clip);
