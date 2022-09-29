cf38ac5258835b56143afcc5eddccc0322c5e196
Madeline Estey <madeline.f.estey@gmail.com>
2022-09-29 01:34:19 -0400

    Comments

--- csf_assign02/c_drawing_funcs.c
+++ csf_assign02/c_drawing_funcs.c
@@ -9,6 +9,12 @@
 // Helper functions
 ////////////////////////////////////////////////////////////////////////
 
+// Blend pixel colors together.
+//
+// Parameters:
+//   foreground    - color of the pixel we are "adding" to the image
+//   background    - color of the pixel already in the image
+//
 uint32_t blur_colors(uint32_t foreground, uint32_t background) {
   uint32_t final_color;
   uint32_t f;
@@ -16,15 +22,23 @@ uint32_t blur_colors(uint32_t foreground, uint32_t background) {
   uint8_t a = (foreground & 255);
   final_color = 255;
 
+  // Compute the correct color value for new pixel (r, g, and b values)
   for(int i = 1; i < 4; i++) {
-    f = ((foreground & (255U << (8*i))) >> (8*i));
+    // Isolate the part of uint32_t that represents the r value, then g...
+    f = ((foreground & (255U << (8*i))) >> (8*i)); 
     b = ((background & (255U << (8*i))) >> (8*i));
     final_color += (((a*f + (255 - a)*b)/255) << (8*i));
   }
-
   return final_color;
 }
 
+// Check if the pixel at (x,y) is valid for the given image.
+//
+// Parameters:
+//   img   - pointer to struct Image
+//   x     - x coordinate (pixel column)
+//   y     - y coordinate (pixel row)
+//
 int is_in_bounds(struct Image *img, int32_t x, int32_t y) {
   int32_t rows = img->height;
   int32_t cols = img->width;
@@ -34,24 +48,57 @@ int is_in_bounds(struct Image *img, int32_t x, int32_t y) {
   return 0;
 }
 
+// Return pixel at specified location (x,y).
+//
+// Parameters:
+//   img   - pointer to struct Image
+//   x     - x coordinate (pixel column)
+//   y     - y coordinate (pixel row)
+//
 uint32_t get_pix(struct Image *img, int32_t x, int32_t y) {
+  // First need to compute location of pixel in data array
   int32_t cols = img->width;
   int32_t location = y*cols + x;
   return img->data[location]; 
 }
 
+// Modifies the color of pixel at specified location (x,y) and BLENDS/BLURS.
+//
+// Parameters:
+//   img   - pointer to struct Image
+//   x     - x coordinate (pixel column)
+//   y     - y coordinate (pixel row)
+//  color  - uint32_t color value
+//
 void set_pix(struct Image *img, int32_t x, int32_t y, int32_t color) {
+  // Compute location of pixel in data array first
   int32_t cols = img->width;
   int32_t location = y*cols + x;
+
+  // Set pixel equal to given color
   img->data[location] = color; 
 }
 
+// Modifies the color of pixel at specified location (x,y) but does NOT blend.
+//
+// Parameters:
+//   img   - pointer to struct Image
+//   x     - x coordinate (pixel column)
+//   y     - y coordinate (pixel row)
+//  color  - uint32_t color value
+//
 void put_pixel(struct Image *img, int32_t x, int32_t y, uint32_t color) {
   if (is_in_bounds(img,x,y)) {
     set_pix(img,x,y,(color|255));
   }
 }
 
+// Ensures the rectangle to be constructed is in the image bounds.
+//
+// Parameters:
+//   img   - pointer to struct Image
+//   tile  - pointer to struct Rect
+//
 int rec_in_bounds(struct Image *img,const struct Rect *tile ) {
   int32_t t_width = tile->width;
   int32_t t_height = tile->height;
@@ -80,7 +127,8 @@ int rec_in_bounds(struct Image *img,const struct Rect *tile ) {
 //
 void draw_pixel(struct Image *img, int32_t x, int32_t y, uint32_t color) {
   if (is_in_bounds(img,x,y)) {
-    uint32_t background = get_pix(img,x,y); //get pix --> locates pixel returns color (set will set color from x,y)
+    // Retrieve pixel color, blur colors, and then set new color
+    uint32_t background = get_pix(img,x,y); 
     background = blur_colors(color,background);
     set_pix(img,x,y,background);
   }
@@ -128,7 +176,9 @@ void draw_circle(struct Image *img,
   int32_t ycorner = y-r;
   for (int i = xcorner; i <= xcorner + 2*r; i++) {
     for (int j = ycorner; j <= ycorner + 2*r; j++) {
+      // Check bounds for program safety
       if (is_in_bounds(img,i,j) == 1) {
+        // Now check to make sure we are drawing in the circle
         int32_t dist = (x-i)*(x-i) + (y-j)*(y-j);
         if(dist <= r*r){
           draw_pixel(img,i,j,color);
@@ -162,8 +212,10 @@ void draw_tile(struct Image *img,
   if (rec_in_bounds(tilemap,tile) == 1) {
     for (int i = x; i<x+width; i++) {
       for (int j = y; j<y+height; j++) {
-        color = get_pix(tilemap,i-x + tile->x,j-y + tile->y); //shift the pix coords back to (0,0) to start
-        put_pixel(img,i,j,color); //map color to coord on main image
+        // Shift the pix coordinates back to (0,0) to start
+        color = get_pix(tilemap,i-x + tile->x,j-y + tile->y); 
+        // Map color to coordinate on main image
+        put_pixel(img,i,j,color); 
       }
     }
   }
@@ -191,8 +243,10 @@ void draw_sprite(struct Image *img, int32_t x, int32_t y, struct Image *spritema
   if (rec_in_bounds(spritemap,sprite) == 1) {
     for (int i = x; i<x+width; i++) {
       for (int j = y; j<y+height; j++) {
-        color = get_pix(spritemap,i-x + sprite->x,j-y + sprite->y); //shift the pix coords back to (0,0) to start
-        draw_pixel(img,i,j,color); //map color to coord on main image
+        // Shift the pix coords back to (0,0) to start
+        color = get_pix(spritemap,i-x + sprite->x,j-y + sprite->y); 
+        // Map color to coord on main image
+        draw_pixel(img,i,j,color); 
       }
     }
   }
