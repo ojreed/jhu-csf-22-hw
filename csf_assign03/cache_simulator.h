50ed0a70c15db617eaf1e4928fe87b6c099926bd
Owen <oreed2@jhu.edu>
2022-10-11 14:42:25 -0400

    reorged classes

--- csf_assign03/cache_simulator.h
+++ csf_assign03/cache_simulator.h
@@ -1,3 +1,6 @@
+#ifndef CACHE_SIMULATOR_H
+#define CACHE_SIMULATOR_H
+
 class cache_simulator
 {
 private:
@@ -8,9 +11,13 @@ private:
     bool write_alloc;
     bool write_thr;
     bool lru;
+
 public:
     cache_simulator(int sets, int blocks, int bytes, bool write_alloc, bool write_thr, bool lru);
     ~cache_simulator();
+    void printResult(std::vector<int> values);
+    std::vector<int> parseTraces();
+
 };
 
 cache_simulator::cache_simulator(int sets, int blocks, int bytes, bool write_alloc, bool write_thr, bool lru)
@@ -27,3 +34,7 @@ cache_simulator::cache_simulator(int sets, int blocks, int bytes, bool write_all
 cache_simulator::~cache_simulator()
 {
 }
+
+
+
+#endif // CACHE_SIMULATOR_H
--- csf_assign03/csim.cpp
+++ csf_assign03/csim.cpp
@@ -7,65 +7,6 @@
 #include "Cache.h"
 
 /*
-TODO:
-    Makefile
-*/
-
-
-void cache_simulator::printResult(std::vector<int> values) {
-    //loads, stores, ldr_hits, ldr_misses, str_hits, str_misses, total
-    //have the lines below
-    std::cout << "Simulation Results: " << "\n";
-    std::cout << "Total loads: " << values[0] << "\n";
-    std::cout << "Total stores: " << values[1] << "\n";
-    std::cout << "Load hits: " << values[2] << "\n";
-    std::cout << "Load misses: " << values[3] << "\n";
-    std::cout << "Store hits: " << values[4] << "\n"; 
-    std::cout << "Store misses: " << values[5] << "\n"; 
-    std::cout << "Total cycles: " << values[6];
-}
-
-std::vector<int> cache_simulator::parseTraces() {
-    std::vector<int> results; //store info in vector
-    //initalizes inputs 
-    int loads, stores, ldr_hits, ldr_misses, str_hits, str_misses, total; 
-    loads = 0;
-    stores = 0;
-    ldr_hits = 0;
-    ldr_misses = 0; 
-    str_hits = 0;
-    str_misses = 0;
-    total = 0;
-
-    //parse to find number of loads and stores first... get that working
-    std::string line;
-    std::string lOrS;
-    while(std::getline(std::cin, line)) {
-        //something like this below, finish later
-        std::istringstream ss(line);
-        //std::getline(ss, lOrS, ' ');
-        ss >> lOrS; //maybe will store first char
-        if(lOrS.compare("l") == 0){
-            loads = loads + 1;
-        } else {
-            stores = stores + 1; 
-        }
-
-    }
-    
-    //stores results to vector
-    results.push_back(loads);
-    results.push_back(stores);
-    results.push_back(ldr_hits);
-    results.push_back(ldr_misses);
-    results.push_back(str_hits);
-    results.push_back(str_misses);
-    results.push_back(total);
-
-    return results;
-}
-
-/*
 Main function of program --> will manage input of command line arguments as well as initalizing the simulation
     Inputs:
         number of sets in the cache (a positive power-of-2)
@@ -144,7 +85,7 @@ int main(int argc, char *argv[]){
     std::cout << "write_thr: " << write_thr << "\n"; 
     std::cout << "lru: " << lru << "\n"; 
 
-
-    printResult(parseTraces(sets,blocks,bytes,write_alloc,write_thr,lru));
+    cache_simulator csim(sets,blocks,bytes,write_alloc,write_thr,lru);
+    csim.printResult(csim.parseTraces());
     return 1;
 }
