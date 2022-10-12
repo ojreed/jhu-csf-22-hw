5c7d5e8c9ac5f7faf8e6a13d8b3ce81aa293f7a2
Owen <oreed2@jhu.edu>
2022-10-12 19:35:02 -0400

    test

--- csf_assign03/Cache.h
+++ csf_assign03/Cache.h
@@ -7,17 +7,14 @@ class Cache {
 
     public:
         Cache(int sets, int blocks, int bytes, bool write_alloc, bool write_thr, bool lru); //default constructor
-<<<<<<< HEAD
         int access(uint32_t adddress,char instruction);
         int load(uint32_t adddress);
         int store(uint32_t adddress);
         //more stuff
-=======
 
         std::vector<Set> getCache() {
             return cache;
         }
->>>>>>> df2c082de13c787ec0ceafca56202b80f96f0f68
 
     private:
         //stuff
