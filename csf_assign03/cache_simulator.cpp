bf6e41f808190b5c88128b9210bfe384f41ae157
Owen <oreed2@jhu.edu>
2022-10-22 18:46:29 -0400

    correcting cycle counter multiplier

--- csf_assign03/cache_simulator.cpp
+++ csf_assign03/cache_simulator.cpp
@@ -114,7 +114,7 @@ std::vector<int> cache_simulator::parseTraces()
             str_misses++;
         }
     }
-    total = (*cache_ctr) * 10 + (*mem_ctr) * 100;
+    total = (*cache_ctr) * 1 + (*mem_ctr) * 100;
     // stores results to vector
     results.push_back(loads);
     results.push_back(stores);
@@ -144,5 +144,6 @@ Load hits: 216821
 Load misses: 101376
 Store hits: 151658
 Store misses: 45828
+Total cycles: 22833960
 
 */
