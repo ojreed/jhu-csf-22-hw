1620f16f5330060a1f6cf61d764e0eb5f3129c44
Owen <oreed2@jhu.edu>
2022-10-24 14:32:20 -0400

    python tester

--- csf_assign03/csim_tester.py
+++ csf_assign03/csim_tester.py
@@ -36,6 +36,9 @@ for file in files:
 				for LRU in range(0,1):
 					for write_alloc in range(0,1):
 						for write_thr in range(0,1):
-							inp = subprocess.run(["./csim", str(Set), str(Block), str(Byte), str(get_alloc(write_alloc)), str(get_thr(write_thr)), str(get_LRU(LRU)), " < " + str(file)], shell=True, check=True)
+							try:
+								inp = subprocess.run(["./csim", str(Set), str(Block), str(Byte), str(get_alloc(write_alloc)), str(get_thr(write_thr)), str(get_LRU(LRU)), " < " + str(file)], shell=True, check=True)
+							raise:
+								raise RuntimeError()
 							print(inp)
 							# results[Set][Block][Byte][write_alloc][write_thr][LRU] = inp.split()[-1]
