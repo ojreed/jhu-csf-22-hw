f5d1a2e2f60f4470f4972c702f7bf3f02071b44e
Owen <oreed2@jhu.edu>
2022-10-24 11:33:36 -0400

    test

--- csf_assign03/Cache.cpp
+++ csf_assign03/Cache.cpp
@@ -124,7 +124,7 @@ int Cache::store(uint32_t address)
          (*target_set).pull_mem(tag, index, offset, current_ts,0);
          // write data to cache
          // (*cache_ctr)++; // increment the number of accesses to cache
-         (*miss_mem_ctr)++;
+         (*mem_ctr)++;
       }
       else // No Write Alloc (doesnt bother to pull mem)
       {
