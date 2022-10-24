9f2358a927405fb998006484c5b9f8f0ac4fd490
Owen <oreed2@jhu.edu>
2022-10-24 11:50:39 -0400

    test

--- csf_assign03/Cache.cpp
+++ csf_assign03/Cache.cpp
@@ -101,14 +101,14 @@ int Cache::store(uint32_t address)
       if (this->write_thr) // write through (Update Cache and access memory)
       {                    // write to memory immediately
          // write to cache
-         (*cache_ctr)++; // increment the number of accesses to cache
+         // (*cache_ctr)++; // increment the number of accesses to cache
          // write to mem
          (*miss_mem_ctr)++; // increment the number of accesses to mem
       }
       else // write back (dont modify memory until overwrite)
       {
          // write to cache
-         (*cache_ctr)+=bytes/4; // increment the number of accesses to cache
+         (*cache_ctr)++; // increment the number of accesses to cache
          // do not write to mem --> defer to replacment
          Slot *slot = (*target_set).get_slot(tag, offset, current_ts);
          (*slot).set_diff_from_mem(true);
