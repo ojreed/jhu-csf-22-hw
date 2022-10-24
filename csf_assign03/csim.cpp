229c7e1bbd056d2e7ac3b73ec2b91f588ec73491
Owen <oreed2@jhu.edu>
2022-10-24 13:49:35 -0400

    test

--- csf_assign03/csim.cpp
+++ csf_assign03/csim.cpp
@@ -32,6 +32,78 @@ If the configuration parameters are invalid, the program should
 
 */
 
+int validate_bools(std::vector<std::string> params,bool *write_alloc,bool *write_thr,bool *lru){
+    // write alloc
+    if (params[4].compare("write-allocate") == 0)
+    {
+        *write_alloc = 1;
+    }
+    else if (params[4].compare("no-write-allocate") == 0)
+    {
+        *write_alloc = 0;
+    }
+    else
+    {
+        std::cerr << "Invalid Input Param for Write_Alloc" << std::endl;
+        return 1;
+    }
+    // write through
+    if (params[5].compare("write-through") == 0)
+    {
+        *write_thr = 1;
+    }
+    else if (params[5].compare("write-back") == 0)
+    {
+        *write_thr = 0;
+    }
+    else
+    {
+        std::cerr << "Invalid Input Param for Write_Thr" << std::endl;
+        return 1;
+    }
+    // lru
+    if (params[6].compare("lru") == 0)
+    {
+        *lru = 1;
+    }
+    else if (params[6].compare("fifo") == 0)
+    {
+        *lru = 0;
+    }
+    else
+    {
+        std::cerr << "Invalid Input Param for LRU" << std::endl;
+        return 1;
+    }
+}
+
+int validate_ints(std::vector<std::string> params, int *sets, int *blocks, int *bytes) {
+    try
+    {
+        // pull inputs and convert string to ints
+        *sets = std::stoi(params[1]);
+        *blocks = std::stoi(params[2]);
+        *bytes = std::stoi(params[3]);
+    }
+    catch (const std::exception &e) // catch invalid input errors for sets, blocks, and bytes
+    {
+        std::cerr << e.what() << " Cache Size Parameter Not an Int" << '\n';
+        return 1;
+    }
+    // validate power of 2
+    if ((*sets % 2 != 0 && *sets != 1 ) || (*blocks % 2 != 0  && *blocks != 1) || (*bytes % 2 != 0))
+    {
+        std::cerr << "Input is not a power of two" << std::endl;
+        return 1;
+    }
+    // validate pos
+    if ((*sets <= 0) || (*blocks <= 0) || (*bytes < 4))
+    {
+        std::cerr << "Input is not of valid size" << std::endl;
+        return 1;
+    }
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -51,74 +123,18 @@ int main(int argc, char *argv[])
     }
     // parse input for set block and byte size
     int sets, blocks, bytes;
-    try
-    {
-        // pull inputs and convert string to ints
-        sets = std::stoi(params[1]);
-        blocks = std::stoi(params[2]);
-        bytes = std::stoi(params[3]);
-    }
-    catch (const std::exception &e) // catch invalid input errors for sets, blocks, and bytes
-    {
-        std::cerr << e.what() << " Cache Size Parameter Not an Int" << '\n';
+    int invalid = validate_ints(params, &sets, &blocks, &bytes);
+    if (invalid == 1) {
         return 1;
     }
-    // validate power of 2
-    if ((sets % 2 != 0 && sets != 1 ) || (blocks % 2 != 0  && blocks != 1) || (bytes % 2 != 0))
-    {
-        std::cerr << "Input is not a power of two" << std::endl;
-        return 1;
-    }
-    // validate pos
-    if ((sets <= 0) || (blocks <= 0) || (bytes < 4))
-    {
-        std::cerr << "Input is not of valid size" << std::endl;
-        return 1;
-    }
+
     // parse input for cache usage modes
     bool write_alloc, write_thr, lru;
-    // write alloc
-    if (params[4].compare("write-allocate") == 0)
-    {
-        write_alloc = 1;
-    }
-    else if (params[4].compare("no-write-allocate") == 0)
-    {
-        write_alloc = 0;
-    }
-    else
-    {
-        std::cerr << "Invalid Input Param for Write_Alloc" << std::endl;
+    invalid = validate_bools(params, &write_alloc, &write_thr, &lru);
+    if (invalid == 1) {
         return 1;
     }
-    // write through
-    if (params[5].compare("write-through") == 0)
-    {
-        write_thr = 1;
-    }
-    else if (params[5].compare("write-back") == 0)
-    {
-        write_thr = 0;
-    }
-    else
-    {
-        std::cerr << "Invalid Input Param for Write_Thr" << std::endl;
-        return 1;
-    }
-    // lru
-    if (params[6].compare("lru") == 0)
-    {
-        lru = 1;
-    }
-    else if (params[6].compare("fifo") == 0)
-    {
-        lru = 0;
-    }
-    else
-    {
-        std::cerr << "Invalid Input Param for LRU" << std::endl;
-        return 1;
-    }
+    
     //create a cache_simulator class with the validated parameters
     cache_simulator csim(sets, blocks, bytes, write_alloc, write_thr, lru);
     //print out the results from parsing and executing the trace file
