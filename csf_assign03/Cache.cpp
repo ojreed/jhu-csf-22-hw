aa95a2e939892cf9e6668bfc0f8fcbe925b52cab
Owen <oreed2@jhu.edu>
2022-10-23 23:59:20 -0400

    test

--- csf_assign03/Cache.cpp
+++ csf_assign03/Cache.cpp
@@ -80,7 +80,7 @@ int Cache::load(uint32_t address)
    {
       // current_ts++;
       // (*cache_ctr)++;                                         // increment the number of accesses to cache
-      (*target_set).pull_mem(tag, index, offset, current_ts,0); // find the oldest element (by mode) and load value from DRAM to that block
+      (*target_set).pull_mem(tag, index, offset, current_ts,1); // find the oldest element (by mode) and load value from DRAM to that block
       return 0;
    }
 }
