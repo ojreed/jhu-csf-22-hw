99fff26841134d4cb50e6f6bcf7fc2c3d9757737
Owen <oreed2@jhu.edu>
2022-10-25 16:07:44 -0400

    python edits

--- csf_assign03/csim_tester.py
+++ csf_assign03/csim_tester.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 import subprocess
 
-byte_range = [4,50]
+byte_size = 4
 block_range = [4,50]
 set_range = [4,50]
 files = ["gcc.trace"]
@@ -32,12 +32,11 @@ results = np.zeros(shape = (50,50,50,2,2,2))
 for file in files:
 	with open(file, 'rb') as f:
 		data = f.read()
-	for Byte in range(byte_range[0],byte_range[1]):
-		for Block in range(block_range[0],block_range[1]):
-			for Set in range(set_range[0],set_range[1]):
-				for LRU in range(0,1):
-					for write_alloc in range(0,1):
-						for write_thr in range(0,1):
-							inp = subprocess.run(["./csim", str(Set), str(Block), str(Byte), str(get_alloc(write_alloc)), str(get_thr(write_thr)), str(get_LRU(LRU))], input = data, shell=False, check=False)
-							print(inp)
-							# results[Set][Block][Byte][write_alloc][write_thr][LRU] = inp.split()[-1]
+	for Block in range(block_range[0],block_range[1]):
+		for Set in range(set_range[0],set_range[1]):
+			for LRU in range(0,1):
+				for write_alloc in range(0,1):
+					for write_thr in range(0,1):
+						inp = subprocess.run(["./csim", str(Set), str(Block), str(Byte), str(get_alloc(write_alloc)), str(get_thr(write_thr)), str(get_LRU(LRU))], input = data, output = out, shell=False, check=False)
+						print("RESULTS" out)
+						# results[Set][Block][Byte][write_alloc][write_thr][LRU] += 1/int(inp.split()[-1])
