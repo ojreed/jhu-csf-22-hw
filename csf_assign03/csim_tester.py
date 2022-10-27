578c59fec2a5ba29146fbdf8ee0ff64a93bddd52
Owen <oreed2@jhu.edu>
2022-10-26 20:24:12 -0400

    test

--- csf_assign03/csim_tester.py
+++ csf_assign03/csim_tester.py
@@ -2,8 +2,8 @@ import numpy as np
 import os
 import subprocess
 import pickle
+total_size = 16000
 byte_size = 4
-block_range = [1,20,1]
 set_range = [1,500,1]
 files = ["gcc.trace","swim.trace"]
 
@@ -27,23 +27,25 @@ def get_LRU(inp):
 		return "fifo"
 
 
-results = np.zeros(shape = (set_range[1],block_range[1],2,2,2))
-# print(results)
+results = np.zeros(shape = (set_range[1],set_range[1],2,2,2))
+print("Total Size: " + str(total_size) + "bytes")
 for file in files:
 	with open(file, 'rb') as f:
 		data = f.read()
 	print("File: " + file)
-	for Block in range(block_range[0],block_range[1],block_range[2]):
+	for Set in range(set_range[0],set_range[1],set_range[2]):
+		Block = (Set*byte_size)/total_size
+		print("Num Sets: " + str(Set))
 		print("Num Blocks: " + str(Block))
-		for Set in range(set_range[0],set_range[1],set_range[2]):
-			print("Num Sets: " + str(Set))
-			for LRU in range(0,1):
-				for write_alloc in range(0,1):
-					for write_thr in range(0,1):
-						return_data = subprocess.run(["./csim", str(Set), str(Block), str(byte_size), str(get_alloc(write_alloc)), str(get_thr(write_thr)), str(get_LRU(LRU))], input = data, capture_output = True, shell=False, check=False)
-						output = return_data.stdout.split()
-						if len(output) > 0:
-							# print(1/int(output[-1]))
-							results[Set][Block][write_alloc][write_thr][LRU] += (int(output[-1]))
+		for LRU in range(0,1):
+			for write_alloc in range(0,1):
+				for write_thr in range(0,1):
+					return_data = subprocess.run(["./csim", str(Set), str(Block), str(byte_size), str(get_alloc(write_alloc)), str(get_thr(write_thr)), str(get_LRU(LRU))], input = data, capture_output = True, shell=False, check=False)
+					output = return_data.stdout.split()
+					if len(output) > 0:
+						# print(1/int(output[-1]))
+						results[Set][Block][write_alloc][write_thr][LRU] += (int(output[-1]))
 print(results)
+k = 10
+np.argpartition(arr, len(arr) - k)[-k:]
 pickle.dump(results , open( "results.p", "wb" ) )
