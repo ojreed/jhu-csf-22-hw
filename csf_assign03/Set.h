532d63fa357fcb0c011389c58dcbfa0e4d8ab1fd
Madeline Estey <madeline.f.estey@gmail.com>
2022-10-29 12:15:05 -0400

    Trying to fix valgrind

--- csf_assign03/Set.h
+++ csf_assign03/Set.h
@@ -1,5 +1,6 @@
 #ifndef SET_H
 #define SET_H
+#include <vector>
 #include "Slot.h"
 
 class Set
@@ -7,6 +8,7 @@ class Set
 public:
     Set(uint32_t blocks, uint32_t bytes, bool write_alloc, bool write_thr, bool lru, uint32_t *cache_ctr, uint32_t *mem_ctr, uint32_t *miss_mem_ctr) : blocks(blocks),bytes(bytes),write_alloc(write_alloc),write_thr(write_thr),lru(lru),cache_ctr(cache_ctr),mem_ctr(mem_ctr),miss_mem_ctr(miss_mem_ctr)
     {
+        std::vector<Slot> set = std::vector<Slot>();
         for (uint32_t x = 0; x < blocks; x++)
         {
             set.push_back(Slot(bytes, write_alloc, write_thr, lru));
