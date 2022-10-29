13127661dc784977460f05decf86b9a78f4c065d
Owen <oreed2@jhu.edu>
2022-10-29 11:33:06 -0400

    python

--- csf_assign03/results_printer.py
+++ csf_assign03/results_printer.py
@@ -1,3 +1,15 @@
 import pickle
+
+
+def printout(results):
+	for ind in range(len(results)):
+		print("Option #" + str(ind))
+		print("	Sets: ", results[ind][1][0])
+		print("	Blocks: ", results[ind][1][1])
+		print("	Allocation: ", get_alloc(results[ind][1][2]))
+		print("	Thr/Bck: ", get_thr(results[ind][1][3]))
+		print("	Mode: ", get_LRU(results[ind][1][4]))
+		print("	Score: ", results[ind][0])
+
 frormatted = pickle.load(open( "results.p", "rb" ) )
 printout(formatted)
