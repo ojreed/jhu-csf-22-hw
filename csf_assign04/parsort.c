831b6b0dc94e3fb3ce12bfb3bdd973b681b78acb
Owen <oreed2@jhu.edu>
2022-11-07 21:38:32 -0500

    trying new fork bs

--- csf_assign04/parsort.c
+++ csf_assign04/parsort.c
@@ -124,6 +124,7 @@ void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
     pid_t pid_r;
     pid_t pid_l = fork();
     if (pid_l == -1) { //left fork handler
+      perror("Left Broke");
       exit(-1);//error case
     } else if (pid_l == 0) {
       merge_sort(arr, mid, end, threshold);
@@ -131,6 +132,7 @@ void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
     } else { //right fork handler
       pid_r = fork();
       if (pid_r == -1) {
+        perror("Right Broke");
         exit(-1);//error case
       } else if (pid_r == 0) {
         merge_sort(arr, begin, mid, threshold);
