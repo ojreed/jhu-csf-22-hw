42dd75d3a039dc1ab489c6ad3110f511581a7f69
Owen <oreed2@jhu.edu>
2022-11-07 21:29:40 -0500

    trying new fork bs

--- csf_assign04/parsort.c
+++ csf_assign04/parsort.c
@@ -130,6 +130,20 @@ void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
       merge_sort(arr, mid, end, threshold);
       exit(0);
     } else { //right fork handler
+      int wstatus;
+      //handle left
+      // blocks until the process indentified by pid_to_wait_for completes
+      pid_t actual_pid_l = waitpid(pid_l, &wstatus, 0);
+      if (!WIFEXITED(wstatus)) {
+        // subprocess crashed, was interrupted, or did not exit normally
+        // handle as error
+        exit(1);
+      }
+      if (WEXITSTATUS(wstatus) != 0) {
+        // subprocess returned a non-zero exit code
+        // if following standard UNIX conventions, this is also an error
+        exit(1);
+      }
       pid_r = fork();
       if (pid_r == -1) {
         exit(1);//error case
@@ -139,20 +153,7 @@ void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
       }  
     }  
 
-    int wstatus;
-    //handle left
-    // blocks until the process indentified by pid_to_wait_for completes
-    pid_t actual_pid_l = waitpid(pid_l, &wstatus, 0);
-    if (!WIFEXITED(wstatus)) {
-      // subprocess crashed, was interrupted, or did not exit normally
-      // handle as error
-      exit(1);
-    }
-    if (WEXITSTATUS(wstatus) != 0) {
-      // subprocess returned a non-zero exit code
-      // if following standard UNIX conventions, this is also an error
-      exit(1);
-    }
+    
     //handle right
     pid_t actual_pid_r = waitpid(pid_r, &wstatus, 0);
     if (!WIFEXITED(wstatus)) {
