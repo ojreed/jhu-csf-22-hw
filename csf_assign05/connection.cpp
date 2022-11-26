b2c34ae406113ab9d660f36bc60b1db1e6c7efc0
Madeline Estey <madeline.f.estey@gmail.com>
2022-11-26 16:29:09 -0500

    Starting work on connection class

--- csf_assign05/connection.cpp
+++ csf_assign05/connection.cpp
@@ -13,35 +13,40 @@ Connection::Connection()
 Connection::Connection(int fd)
   : m_fd(fd)
   , m_last_result(SUCCESS) {
-  // TODO: call rio_readinitb to initialize the rio_t object
+  // Call rio_readinitb to initialize the rio_t object
+  rio_t *rp;
+  rio_readinitb(rp, fd);
 }
 
 void Connection::connect(const std::string &hostname, int port) {
-  // TODO: call open_clientfd to connect to the server
-  // TODO: call rio_readinitb to initialize the rio_t object
+  // Call open_clientfd to connect to the server
+  int fd = open_clientfd((const char&)hostname, (const char*)port); // Error for some reason on the second (
+  // Call rio_readinitb to initialize the rio_t object
+  rio_t *rp;
+  rio_readinitb(rp, fd);
 }
 
 Connection::~Connection() {
-  // TODO: close the socket if it is open
+  // Close the socket if it is open
   
 }
 
 bool Connection::is_open() const {
-  // TODO: return true if the connection is open
+  // Return true if the connection is open
 }
 
 void Connection::close() {
-  // TODO: close the connection if it is open
+  // Close the connection if it is open
 }
 
 bool Connection::send(const Message &msg) {
-  // TODO: send a message
+  // Send a message
   // return true if successful, false if not
   // make sure that m_last_result is set appropriately
 }
 
 bool Connection::receive(Message &msg) {
-  // TODO: receive a message, storing its tag and data in msg
+  // Receive a message, storing its tag and data in msg
   // return true if successful, false if not
   // make sure that m_last_result is set appropriately
 }
