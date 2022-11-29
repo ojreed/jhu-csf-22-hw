132c74e6e246a121232e802309a876fb4da3ed96
Madeline Estey <madeline.f.estey@gmail.com>
2022-11-28 21:41:36 -0500

    String issue

--- csf_assign05/receiver.cpp
+++ csf_assign05/receiver.cpp
@@ -37,9 +37,9 @@ int main(int argc, char **argv) {
   // login_message += "\r\n";
   char const* formatted = login_message.c_str();
   Rio_writen(fd, formatted, strlen(formatted));
-  struct Message response;
+  char* response;
   rio_t rio_response; 
-  Rio_readlineb(&rio_response, &response, 225); // Rio_readlineb might be sufficient error-wise actually...
+  Rio_readlineb(&rio_response, response, 225); // Rio_readlineb might be sufficient error-wise actually...
   // Listen for okay from server 
   //if(response.tag == "err") {
     //perror("Error...");
