af6183411ed6215cc92fd80a77458f88a9d0c8b1
Madeline Estey <madeline.f.estey@gmail.com>
2022-12-05 22:44:08 -0500

    Work on room

--- csf_assign05/room.cpp
+++ csf_assign05/room.cpp
@@ -7,18 +7,32 @@
 Room::Room(const std::string &room_name)
   : room_name(room_name) {
   // TODO: initialize the mutex
+  pthread_mutex_init(&lock, NULL);
+  //Guard *og = new Guard(lock);
 }
 
 Room::~Room() {
   // TODO: destroy the mutex
+  //delete og;
+  pthread_mutex_destroy(&lock);
 }
 
 void Room::add_member(User *user) {
   // TODO: add User to the room
+  std::set<User *>::iterator it = members.find(user);
+  if (it != members.end()) {
+    // Element was found
+  } else {
+    Guard *g = new Guard(this->lock); //protect access while adding user
+    this->members.insert(user);
+    delete g;
+  }
 }
 
 void Room::remove_member(User *user) {
   // TODO: remove User from the room
+  std::set<User *>::iterator it = members.find(user);
+  members.erase(it);
 }
 
 void Room::broadcast_message(const std::string &sender_username, const std::string &message_text) {
